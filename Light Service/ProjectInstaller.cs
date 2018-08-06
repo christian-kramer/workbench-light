5852ee2642a8e1945af60cf8c6f3d019c766a822
Christian Kramer <39014541+christian-kramer@users.noreply.github.com>
2018-08-05 21:17:02 -0500

    Initial Commit

---
+++ Light Service/ProjectInstaller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace Light_Service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            this.AfterInstall += new InstallEventHandler(ServiceInstaller_AfterInstall);
+        }
+
+        private void ServiceInstaller_AfterInstall(object sender, InstallEventArgs e)
+        {
+            using (ServiceController sc = new ServiceController(serviceInstaller1.ServiceName))
+            {
+                sc.Start();
+            }
+        }
+
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Light Service/Service1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Light_Service
+{
+    public class Outlet
+    {
+        internal static void Turn(string state)
+        {
+            RegistryKey options = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Services\\Workbench Light");
+            string token = options.GetValue("token").ToString();
+            string account = options.GetValue("accountid").ToString();
+            string outlet = options.GetValue("outletid").ToString();
+
+            string url = $"https://smartapi.vesync.com/v1/wifi-switch-1.3/{outlet}/status/{state}";
+            
+            using (var client = new System.Net.WebClient())
+            {
+                client.Headers.Add("tk", token);
+                client.Headers.Add("accountid", account);
+
+                bool success = false;
+                int tries = 0;
+                while (success == false && tries < 10)
+                {
+                    try
+                    {
+                        success = true;
+                        client.UploadData(url, "PUT", Encoding.UTF8.GetBytes("{}"));
+                        tries++;
+                    }
+                    catch (System.Net.WebException)
+                    {
+                        success = false;
+                    }
+                    System.Threading.Thread.Sleep(3000);
+                }
+            }
+        }
+    }
+
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            this.CanHandlePowerEvent = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //Outlet.Turn("on");
+        }
+
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            if (powerStatus.HasFlag(PowerBroadcastStatus.QuerySuspend))
+            {
+                Outlet.Turn("off");
+            }
+
+            if (powerStatus.HasFlag(PowerBroadcastStatus.ResumeSuspend))
+            {
+                Outlet.Turn("on");
+            }
+            return base.OnPowerEvent(powerStatus);
+        }
+
+        protected override void OnStop()
+        {
+            //Outlet.Turn("off");
+        }
+    }
+}
